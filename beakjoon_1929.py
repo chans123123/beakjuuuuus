8a48c67be8462b706aa326d43ec9ab1a4ea8e0ff
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-07 23:50:41 +0900

    Create beakjoon_1929.py

---
+++ beakjoon_1929.py
@@ -0,0 +1,11 @@
+import sys
+
+M, N = map(int, sys.stdin.readline().split())
+for i in range(M, N+1):
+    if i == 1:
+        continue
+    for j in range(2,int(i**0.5)+1):
+        if i % j == 0:
+            break
+    else:
+        print(i)
