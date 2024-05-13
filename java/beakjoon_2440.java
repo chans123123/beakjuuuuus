7fc5811b1f7b3623f43e613ce8ff549c7017608b
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-05-13 21:11:56 +0900

    Create beakjoon_2440.java

---
+++ java/beakjoon_2440.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
