693ff5bfaeb8b793a3e850650cef17b89dfefe43
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-06-05 19:43:42 +0900

    Update beakjoon_11047.py

--- beakjoon_11047.py
+++ beakjoon_11047.py
@@ -5,3 +5,13 @@ n, k = map(int,input().split())
 coin_list = []
 cnt = 0
 
+for _ in range(n):
+    coin = int(input())
+    coin_list.append(coin)
+
+coin_list.sort(reverse=True)
+for i in range(len(coin_list)):
+    cnt += k // coin_list[i]
+    k %= coin_list[i]
+
+print(cnt)
