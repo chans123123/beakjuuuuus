10abd9af9b8cd17072877f987fff782fb49e6020
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-07-08 09:10:59 +0900

    Update beakjoon_1110.java

--- beakjoon_1110.java
+++ beakjoon_1110.java
@@ -23,22 +23,3 @@ public class Main {
         System.out.println(cnt);
     }
 }
-
-import sys
-input = sys.stdin.readline
-
-n = int(input()) #26
-num = n
-cnt = 0
-
-while True:
-    a = num // 10 #2
-    b = num % 10  #6
-    c = (a + b) % 10   #8
-    num = (b*10) + c
-    cnt += 1
-    
-    if n == num:
-        break
-
-print(cnt)
