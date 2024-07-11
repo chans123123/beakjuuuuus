7822c5f0d6318daec4553b3c370ff90fd9fd24b7
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-07-11 14:05:23 +0900

    Create beakjoon_1330.java

---
+++ java/beakjoon_1330.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+}
