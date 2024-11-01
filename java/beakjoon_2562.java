fcf99d2c5a95cad8a5a0782d96ad81c3133cdb2c
chans123123 <chanyounng6@gmail.com>
2024-11-01 15:35:06 +0900

    11/01

---
+++ java/beakjoon_2562.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class beakjoon_2562 {
+    public static void main(String[] args) {
+        int [] numbers = new int[9];
+        for (int i = 0; i < 9; i++) {
+            Scanner scanner = new Scanner(System.in);
+
+        }
+    }
+}
