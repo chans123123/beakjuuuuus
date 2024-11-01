d8ee799904fb4cf2c90e9278a1ba13d7d72b2564
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-01 21:30:31 +0900

    Update beakjoon_2562.java

--- java/beakjoon_2562.java
+++ java/beakjoon_2562.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 
-public class beakjoon_2562 {
+public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int [] numbers = new int[9];
-        for (int i = 0; i < 9; i++) {
-            Scanner scanner = new Scanner(System.in);
-
+        int max = numbers[0];
+        int index = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
         }
+        scanner.close();
+        for (int i = 0; i < numbers.length; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];
+                index = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
     }
 }
